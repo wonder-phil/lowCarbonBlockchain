49bed88c4c73e595ad96db9b2a67b0644c483408
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2024-04-01 16:55:43 -0400

    Update part3-getNetworkDetails.sh
    add install for skupper 
    rolesetup -> roleSetup

--- installScripts/part3-getNetworkDetails.sh
+++ installScripts/part3-getNetworkDetails.sh
@@ -6,6 +6,10 @@ kubectl wait --namespace metallb-system                 --for=condition=ready po
 docker network inspect -f '{{.IPAM.Config}}' kind
 # This will get us the 
 
+curl https://skupper.io/install.sh | sh
+
+
+
 
 
 kubectl apply -f metal.yaml
@@ -15,13 +19,14 @@ skupper init -n ns1
 skupper init -n ns2
 skupper token create ./SECRET.yaml --uses 100 -n ns1
 skupper link create ./SECRET.yaml -n ns2
-kubectl apply -f ../rolesetup/serviceacc.yaml --namespace ns1
-kubectl apply -f ../rolesetup/serviceacc.yaml --namespace ns2
-kubectl apply -f ../rolesetup/rolebinder.yaml --namespace ns1
-kubectl apply -f ../rolesetup/rolebinder.yaml --namespace ns2
+kubectl apply -f ../roleSetup/serviceacc.yaml --namespace ns1
+kubectl apply -f ../roleSetup/serviceacc.yaml --namespace ns2
+kubectl apply -f ../roleSetup/rolebinder.yaml --namespace ns1
+kubectl apply -f ../roleSetup/rolebinder.yaml --namespace ns2
 kubectl apply -f ../backEndListener/backend-listener-1.yaml --namespace ns1
 kubectl apply -f ../backEndListener/backend-listener-2.yaml --namespace ns2
 kubectl create deployment frontend --image gemajlia/basicwebcontainer:latest --namespace ns1
 kubectl create deployment frontend --image gemajlia/basicwebcontainer:latest --namespace ns2
 kubectl expose deployment/frontend --port 8080 --target-port 80 --type LoadBalancer --namespace ns1
+
 skupper expose deployment/backend-listener-2 -n ns2
