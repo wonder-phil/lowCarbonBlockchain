88d98f1237677628b6ecbec007a86aff8a432d52
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2024-04-30 16:33:19 -0400

    Update part3-getNetworkDetails.sh

--- installScripts/part3-getNetworkDetails.sh
+++ installScripts/part3-getNetworkDetails.sh
@@ -3,6 +3,7 @@ export KUBECONFIG=~/.kube/main
 kind create cluster --name scotch
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
 kubectl wait --namespace metallb-system                 --for=condition=ready pod                 --selector=app=metallb                 --timeout=90s
+sudo chmod 666 /var/run/docker.sock
 docker network inspect -f '{{.IPAM.Config}}' kind
 # This will get us the 
 
